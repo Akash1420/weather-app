c5d52dc7d4a44716d592a9334ff6c7b389c29b43
Akash Tripathi <tripathiaakash14@gmail.com>
2020-06-23 13:53:04 +0530

    update env variable

--- server.js
+++ server.js
@@ -33,6 +33,10 @@ app.post('/', function (req, res) {
   });
 })
 
-app.listen(3000, function () {
-  console.log('Weather app listening on port 3000!')
+// app.listen(3000, function () {
+//   console.log('Weather app listening on port 3000!')
+// })
+
+app.listen(process.env.PORT , process.env.IP, function(){
+  console.log("Server is runnning!!");
 })
